cd2005e282a0289a2aef62278a625fba3c952739
tsg66 <joexiayong@foxmail.com>
2019-05-22 18:51:33 +0800

    change rules of gitignore

--- 1023.c
+++ 1023.c
@@ -40,18 +40,19 @@ void swap(int *a, int *b)
 }
 int main()
 {
-    int a[10], i, j, temp;
-    for (i = 0; i < 10; i++)
-    {
-        scanf("%d", &a[i]);
-    }
+    int i, j, temp;
+//    for (i = 0; i < 10; i++)
+//    {
+//        scanf("%d", &a[i]);
+//    }
+int a[10]={4 ,85  ,3, 234, 45, 345 ,345, 122 ,30, 12};
     for (i = 1; i < 10; i++)
     {
         for (j = 0; j < 10 - i; j++)
         {
             if (a[j] > a[j + 1])
             {
-                swap(a[j],a[j+1]);
+                swap(&a[j],&a[j+1]);
             }
         }
     }
---
+++ daliy/may22th.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main() {
+    float s=2.734;
+    printf("%1.2f\n",s); // 2.73
+    char t[]="\\141\141abc\t";
+    int a=strlen(t); // a:9
+    return 0;
+}
